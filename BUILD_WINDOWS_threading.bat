ee209a3a5683ef26dbf5e80b9718da46c8105851
maoist2009 <179199418+maoist2009@users.noreply.github.com>
2024-12-15 15:28:43 +0800

    upd

--- BUILD_WINDOWS_threading.bat
+++ BUILD_WINDOWS_threading.bat
@@ -1,4 +1,4 @@
-pip install -r requirements.txt
+pip install -r requirements_threading.py.txt
 pyinstaller -F server_threading.py -n proxy.exe --noconsole
 copy /Y config.json dist\config.json
 pause
