2fcad8e1fbb6e0c8ea9253e70cbb58bee8406aea
Sving1024 <sving1024@outlook.com>
2025-04-19 22:21:30 +0800

    rm git ignore entry

---
+++ src/tls_fragment/utils.py
@@ -0,0 +1,72 @@
+import ipaddress
+from tls_fragment.log import logger
+import socket
+
+
+def ip_to_binary_prefix(ip_or_network):
+    try:
+        network = ipaddress.ip_network(ip_or_network, strict=False)
+        network_address = network.network_address
+        prefix_length = network.prefixlen
+        if isinstance(network_address, ipaddress.IPv4Address):
+            binary_network = bin(int(network_address))[2:].zfill(32)
+        elif isinstance(network_address, ipaddress.IPv6Address):
+            binary_network = bin(int(network_address))[2:].zfill(128)
+        binary_prefix = binary_network[:prefix_length]
+        return binary_prefix
+    except ValueError:
+        try:
+            ip = ipaddress.ip_address(ip_or_network)
+            if isinstance(ip, ipaddress.IPv4Address):
+                binary_ip = bin(int(ip))[2:].zfill(32)
+                binary_prefix = binary_ip[:32]
+            elif isinstance(ip, ipaddress.IPv6Address):
+                binary_ip = bin(int(ip))[2:].zfill(128)
+                binary_prefix = binary_ip[:128]
+            return binary_prefix
+        except ValueError:
+            raise ValueError(f"输入 {ip_or_network} 不是有效的 IP 地址或网络")
+
+
+def set_ttl(sock, ttl):
+    if sock.family == socket.AF_INET6:
+        sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_UNICAST_HOPS, ttl)
+    else:
+        sock.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, ttl)
+
+
+def check_ttl(ip, port, ttl):
+    try:
+        if ip.find(":") != -1:
+            sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+        else:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        set_ttl(sock, ttl)
+        sock.settimeout(2)
+        sock.connect((ip, port))
+        sock.send(b"0")
+        sock.close()
+        return True
+    except Exception as e:
+        logger.warning(e)
+        return False
+    finally:
+        sock.close()
+
+
+def get_ttl(ip, port):
+    l = 1
+    r = 128
+    ans = -1
+    while l <= r:
+        mid = (l + r) // 2
+        val = check_ttl(ip, port, mid)
+        logger.debug("%d %d %d %d %d", l, r, mid, ans, val)
+        if val:
+            ans = mid
+            r = mid - 1
+        else:
+            l = mid + 1
+
+    logger.info("get_ttl %s %d %d", ip, port, ans)
+    return ans
