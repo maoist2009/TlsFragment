f239b4651cfd5cf729ea515138c464baae11f389
maoist2009 <maoist2009@outlook.com>
2025-05-14 12:58:48 +0800

    try to fix foregroud service

--- main.py
+++ main.py
@@ -63,21 +63,43 @@ class ProxyApp(App):
     def start_proxy_service(self, instance):
         """启动Android服务来运行代理"""
         try:
-            
-            SERVICE_NAME = u'{packagename}.Service{servicename}'.format(
-                packagename=u'org.maoist2009.tlsfragment',
-                servicename=u'Myservice'
-            )
-            service = autoclass(SERVICE_NAME)
-            mActivity = autoclass(u'org.kivy.android.PythonActivity').mActivity
-            argument = ''
-            
-            # 创建通知
-            notification = self.create_notification()
-            service.startForeground(1, notification)
-            
-            # 启动服务主函数
-            service.start(mActivity, argument)
+            import jnius
+            Context = jnius.autoclass('android.content.Context')
+            Intent = jnius.autoclass('android.content.Intent')
+            PendingIntent = jnius.autoclass('android.app.PendingIntent')
+            AndroidString = jnius.autoclass('java.lang.String')
+            NotificationBuilder = jnius.autoclass('android.app.Notification$Builder')
+            Notification = jnius.autoclass('android.app.Notification')
+            service_name = 'ProxyService'
+            package_name = 'org.maoist2009.tlsfragment'
+            service = jnius.autoclass('org.kivy.android.PythonService').mService
+            # Previous version of Kivy had a reference to the service like below.
+            #service = jnius.autoclass('{}.Service{}'.format(package_name, service_name)).mService
+            PythonActivity = jnius.autoclass('org.kivy.android' + '.PythonActivity')
+            notification_service = service.getSystemService(
+                Context.NOTIFICATION_SERVICE)
+            app_context = service.getApplication().getApplicationContext()
+            notification_builder = NotificationBuilder(app_context)
+            title = AndroidString("EzTunes".encode('utf-8'))
+            message = AndroidString("Ready to play music.".encode('utf-8'))
+            app_class = service.getApplication().getClass()
+            notification_intent = Intent(app_context, PythonActivity)
+            notification_intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
+                Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_NEW_TASK)
+            notification_intent.setAction(Intent.ACTION_MAIN)
+            notification_intent.addCategory(Intent.CATEGORY_LAUNCHER)
+            intent = PendingIntent.getActivity(service, 0, notification_intent, 0)
+            notification_builder.setContentTitle(title)
+            notification_builder.setContentText(message)
+            notification_builder.setContentIntent(intent)
+            Drawable = jnius.autoclass("{}.R$drawable".format(service.getPackageName()))
+            icon = getattr(Drawable, 'icon')
+            notification_builder.setSmallIcon(icon)
+            notification_builder.setAutoCancel(True)
+            new_notification = notification_builder.getNotification()
+            #Below sends the notification to the notification bar; nice but not a foreground service.
+            #notification_service.notify(0, new_noti)
+            service.startForeground(1, new_notification)
             
             print("Proxy service started")
             self.start_button.disabled = True
