35bd4617ff17027bb880bf485fe0746f29d9dd35
maoist2009 <179199418+maoist2009@users.noreply.github.com>
2025-05-17 15:59:10 +0800

    upd202505171559

--- service.py
+++ service.py
@@ -1,26 +1,21 @@
 # service/main.py
 from kivy.logger import Logger
-from kivy.clock import Clock
 import os
 import threading
 from src.tls_fragment.cli import start_server, stop_server
-import plyer
 
-def start():
-    """启动代理服务器并保持服务运行"""
-    Logger.info("Service: Starting proxy server")
+"""启动代理服务器并保持服务运行"""
+Logger.info("Service: Starting proxy server")
+
+try:
+    # 启动代理服务器（非阻塞模式）
+    # 不再传递config参数
+    Logger.info("Service: Proxy server started successfully")
+    start_server(block=True)
     
-    try:
-        # 启动代理服务器（非阻塞模式）
-        # 不再传递config参数
-        Logger.info("Service: Proxy server started successfully")
-        start_server(block=True)
-        
-        # 保持主线程运行，防止服务被系统终止
-        Clock.schedule_interval(lambda dt: None, 1)
-        
-    except Exception as e:
-        Logger.error(f"Service: Error starting server: {str(e)}")
+    # # 保持主线程运行，防止服务被系统终止
+    # Clock.schedule_interval(lambda dt: None, 1)
+    
+except Exception as e:
+    Logger.error(f"Service: Error starting server: {str(e)}")
 
-
-start()
