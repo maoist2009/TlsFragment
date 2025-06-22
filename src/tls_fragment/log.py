bc7be0f10bd4bb8b25142b820ab73ca2ff336a89
maoist2009 <maoist2009@outlook.com>
2025-06-22 20:20:05 +0800

    upd

--- src/tls_fragment/log.py
+++ src/tls_fragment/log.py
@@ -1,9 +1,18 @@
 import logging
 from .config import config
 
-logging.basicConfig(
-    format="%(asctime)s %(levelname)-8s %(name)s: %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-)
+if config.get("logfile"):
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)-8s %(name)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        filemode="w",
+        filename=config["logfile"]
+    )
+else:
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)-8s %(name)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
 logger = logging.getLogger("tls_fragmenter")
 logger.setLevel(config["loglevel"])
